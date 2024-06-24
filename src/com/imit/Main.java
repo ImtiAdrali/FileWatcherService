c6632a087b748fc7f917eb09998e797f60fdb717
Imtiaz Ahmad <imtiazahmadadrali@gmail.com>
2024-06-24 14:56:25 -0400

    removed the path

--- src/com/imit/Main.java
+++ src/com/imit/Main.java
@@ -7,7 +7,7 @@ import com.imit.service.FileWatcherService;
 public class Main {
     public static void main(String[] args) {
         try {
-        	FileWatcherService fileWatcherService = new FileWatcherService("C:\\Users\\IAhmad\\Downloads");
+        	FileWatcherService fileWatcherService = new FileWatcherService("Your path goes here");
         	fileWatcherService.startWatching();
         } catch (IOException e) {
 			System.out.println("Error initializing the watcher server: " + e.getMessage());
